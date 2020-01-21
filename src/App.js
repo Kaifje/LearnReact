9449afb5608d7c96a1b2cd7af8357ea5124bcb6d
kaifjellstadsveen <kaifjellstadsveen@gmail.com>
2020-01-21 22:20:00 +0100

    added card components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { CardList} from './components/card-list/card-list.component'
+
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: "Hello Kaizen"
+      monsters: [],
+      searchField: ''
     };
   }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.setState({monsters: users}));
+     
+  }
   render() {
+    const { monsters, searchField   } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: "Hello" })}>
-            Bytt tekst
-          </button>
-        </header>
+        <input type='search' placeholder='søk' 
+        onChange={e => this.setState({searchField: e.target.value })} />
+         <CardList monsters ={filteredMonsters} />        
       </div>
     );
   }
