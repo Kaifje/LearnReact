12497fc00899971286b3a07795f3d17ef672704c
kaifjellstadsveen <kaifjellstadsveen@gmail.com>
2020-01-25 21:08:42 +0100

    Added title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { Component } from "react";
 import { CardList} from './components/card-list/card-list.component'
+import { SearchBox} from './components/search-box/search-box.component';
+
 
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
       monsters: [],
       searchField: ''
     };
+    // this.handleChange = this.handleChange.bind(this);
+
   }
 
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
-  .then(users => this.setState({monsters: users}));
-     
+  .then(users => this.setState({monsters: users}));     
   }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value })
+  }
+
   render() {
     const { monsters, searchField   } = this.state;
     const filteredMonsters = monsters.filter(monster => 
@@ -26,8 +35,10 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input type='search' placeholder='søk' 
-        onChange={e => this.setState({searchField: e.target.value })} />
+      <h1> Monsters Rolodex </h1>
+        <SearchBox placeholder='Søk monstere'
+          handleChange={this.handleChange}
+        />
          <CardList monsters ={filteredMonsters} />        
       </div>
     );
