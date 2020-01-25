531066001fa2d52101669e432ef4ad98fd715089
kaifjellstadsveen <kaifjellstadsveen@gmail.com>
2020-01-25 21:37:11 +0100

    Updates

---
+++ precache-manifest.aa5b717cb41941e7058d3e38379ffb7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2e899234ab971972991e94b1774b19",
+    "url": "/learnreact/index.html"
+  },
+  {
+    "revision": "527d765160f61b59fe6f",
+    "url": "/learnreact/static/css/main.9a2d4369.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/learnreact/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/learnreact/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "527d765160f61b59fe6f",
+    "url": "/learnreact/static/js/main.d1c4b3f1.chunk.js"
+  },
+  {
+    "revision": "f7355140ebad5e89e48e",
+    "url": "/learnreact/static/js/runtime-main.6ceec59f.js"
+  }
+]);
